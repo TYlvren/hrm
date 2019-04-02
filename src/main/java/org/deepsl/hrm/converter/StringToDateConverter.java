df7e8dc375854aab07f5a424ff9419afc7c076b1
TYlvren <649729063@qq.com>
2019-04-02 19:49:43 +0800

    新加入格式转换器，String →  Date

--- src/main/java/org/deepsl/hrm/converter/StringToDateConverter.java
+++ src/main/java/org/deepsl/hrm/converter/StringToDateConverter.java
@@ -2,12 +2,25 @@ package org.deepsl.hrm.converter;
 
 import org.springframework.core.convert.converter.Converter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class StringToDateConverter implements Converter<String, Date> {
 
     @Override
+    /**
+     * 将字符串输入的日期按照定义的格式转换为Date型
+     */
     public Date convert(String s) {
-        return null;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+
     }
 }
